1eea11ddef45d158276d2ea50dd52249f8720d6f
Harysson Soares <haryssonsoares@gmail.com>
2021-08-16 23:43:20 -0300

    chore: clean code

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <span>Start codeleap frontend project</span>
+    </div>
+  )
+}
+
+export default Home
